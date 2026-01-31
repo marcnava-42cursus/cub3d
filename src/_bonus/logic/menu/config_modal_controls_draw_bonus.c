fadfbd5d4233a604752cd1ea924c07c500117611
marcnava <marcnava@student.42madrid.com>
2026-01-31 16:03:59 +0100

    Changed size of modal box

--- src/_bonus/logic/menu/config_modal_controls_draw_bonus.c
+++ src/_bonus/logic/menu/config_modal_controls_draw_bonus.c
@@ -14,6 +14,13 @@
 #include "structs.h"
 #include "logic_bonus.h"
 
+static int	controls_card_min_height(void)
+{
+	return (CONFIG_MODAL_CARD_PADDING + CONFIG_MODAL_SECTION_TITLE_GAP
+		+ (CONFIG_MODAL_CONTROL_COUNT - 1) * CONFIG_MODAL_ROW_SPACING
+		+ CONFIG_MODAL_ROW_HEIGHT + CONFIG_MODAL_CARD_PADDING);
+}
+
 static void	draw_controls_prompt(t_game *game, t_rect card, int column)
 {
 	t_menu_state	*menu;
@@ -113,12 +120,20 @@ void	draw_controls_options(t_game *game, t_rect panel)
 {
 	t_menu_layout	layout;
 	t_menu_state	*menu;
+	int				min_height;
 
 	if (!game || !game->menu.modal || !ensure_controls_labels(game))
 		return ;
 	menu = &game->menu;
 	(void)panel;
 	layout = menu_layout_build(menu);
+	min_height = controls_card_min_height();
+	if (min_height > layout.content.h)
+		min_height = layout.content.h;
+	if (layout.left.h < min_height)
+		layout.left.h = min_height;
+	if (layout.right.h < min_height)
+		layout.right.h = min_height;
 	draw_controls_column(game, layout.left, CONTROLS_COLUMN_KEYBOARD);
 	draw_controls_column(game, layout.right, CONTROLS_COLUMN_CONTROLLER);
 }
