54a3f97677a19d3942db24817a3084a1867160ab
Ivan Miranda <ivanmirandaart@yahoo.com>
2026-02-08 14:14:38 +0100

    norminette done

--- src/_bonus/animation/door_anim_states.c
+++ src/_bonus/animation/door_anim_states.c
@@ -6,11 +6,41 @@
 /*   By: ivmirand <ivmirand@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 21:29:32 by ivmirand          #+#    #+#             */
-/*   Updated: 2026/02/08 04:09:02 by ivmirand         ###   ########.fr       */
+/*   Updated: 2026/02/08 14:13:39 by ivmirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "animation.h"
+
+static void	when_orb_mode_none(bool finished, t_cub_data *data)
+{
+	if (finished)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_CLOSED);
+}
+
+static void	when_orb_mode_place(bool finished, t_cub_data *data)
+{
+	if (!finished && data->effects.current_door_anim != DOOR_OPEN)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_OPEN);
+	else if (finished && data->effects.current_door_anim == DOOR_OPEN)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_OPENED);
+}
+
+static void	when_orb_mode_take(bool finished, t_cub_data *data)
+{
+	if (!finished && data->effects.current_door_anim != DOOR_OPEN)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_OPEN);
+	else if (finished && data->effects.current_door_anim == DOOR_OPEN)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_CLOSE);
+	else if (finished && data->effects.current_door_anim == DOOR_CLOSE)
+		set_current_anim(data->effects.door_anims,
+			&data->effects.current_door_anim, DOOR_CLOSED);
+}
 
 void	update_door_anims(t_cub_data *data, t_orb_projectile *orb,
 		float delta_time)
@@ -20,30 +50,9 @@ void	update_door_anims(t_cub_data *data, t_orb_projectile *orb,
 	finished = anim_update(&data->effects.door_anims[
 			data->effects.current_door_anim], delta_time);
 	if (orb->mode == ORB_MODE_NONE)
-	{
-		if (finished)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_CLOSED);
-	}
+		when_orb_mode_none(finished, data);
 	else if (orb->mode == ORB_MODE_PLACE)
-	{
-		if (!finished && data->effects.current_door_anim != DOOR_OPEN)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_OPEN);
-		else if (finished && data->effects.current_door_anim == DOOR_OPEN)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_OPENED);
-	}
+		when_orb_mode_place(finished, data);
 	else if (orb->mode == ORB_MODE_TAKE)
-	{
-		if (!finished && data->effects.current_door_anim != DOOR_OPEN)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_OPEN);
-		else if (finished && data->effects.current_door_anim == DOOR_OPEN)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_CLOSE);
-		else if (finished && data->effects.current_door_anim == DOOR_CLOSE)
-			set_current_anim(data->effects.door_anims,
-				&data->effects.current_door_anim, DOOR_CLOSED);
-	}
+		when_orb_mode_take(finished, data);
 }
